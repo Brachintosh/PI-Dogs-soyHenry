a3ce0d6e7ec5754e0945158f3392f49d1292e201
brachintosh <bedendo.br@gmail.com>
2021-11-23 17:28:00 -0300

    dog-routes-get

--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -10,9 +10,32 @@ router.get('/get', async (req, res, next) => {
         const totalBreeds = await getRazas();
         // Busqueda por query name:
         if(req.query.name) {
-            
+            let breed = await totalBreeds.filter((br) => 
+            br.name.toLowerCase().includes(req.query.name.toLowerCase())
+            )
+            if(breed.length > 0) return res.status(200).send(breed);
+            res.status(404).send("Breed not found...");
         }
+        console.log(breed)
+        res.status(200).send(totalBreeds);
 
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get('/:breedId', async(req, res, next) => {
+    try {
+
+        const totalBreeds = await getRazas();
+        // Busqueda por query params >>> Id:
+        const breedId = req.params.breedId
+
+        let breed = totalBreeds.filter((br) => br.id.toString() === breedId);
+
+        if(breed.length > 0) return res.status(200).send(breed);
+
+        res.status(404).send("No breed matches that ID");
     } catch (error) {
         next(error)
     }
