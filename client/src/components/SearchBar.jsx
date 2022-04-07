21a4cf892b3ff9887911e7bbabc464b91b73fd29
brachintosh <bedendo.br@gmail.com>
2022-04-07 19:08:25 -0300

    actions

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import  { useDispatch } from "react-redux"
-import  { getByQueryName }from "../../redux/actions/index";
+import  { getByQueryName }from "../redux/actions/index";
 import './SearchBar/search.css';
 
 export default function SearchBar({paginaLocal}) {
